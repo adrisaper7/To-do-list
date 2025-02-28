43a31d26e27707ffc49248c48a05939760c25fed
Alistor6 <erik.rango2004@gmail.com>
2025-02-28 12:12:41 +0100

    Orden

--- control.js
+++ control.js
@@ -35,21 +35,6 @@ function crear_nota(numero_nota, titulo, clase, color, array_notas){
     return array_notas
 }
 
-function ordenarTarjetas() {
-    let idOrdenar = document.getElementById("ordenar");
-    
-
-
-    if(idOrdenar.textContent === "ordenar por fecha"){
-        idOrdenar.textContent = "ordenar por color";
-
-    }
-    else{
-        idOrdenar.textContent = "ordenar por fecha";
-    }
-}
-
-document.getElementById("ordenar").addEventListener("click", ordenarTarjetas);
 
 //Obtener fecha actual
 function crearFechaYhora(){
@@ -512,26 +497,30 @@ guardar_en_local(array_notas)
 mostrar_de_array(array_notas)
 
 boton_ordenar.addEventListener("click", function() {
-    ordenar = (ordenar+1)%3
+    ordenar = (ordenar+1)%4
     if (ordenar == 0){ //ordenar por numero de nota
         array_notas.sort((a,b) => b[0] - a[0])
         guardar_en_local(array_notas)
         mostrar_de_array(array_notas)
+        boton_ordenar.textContent = "Mas nueva"
     } 
     else if (ordenar == 1){ //ordenar por titulo
         array_notas.sort((a,b) => a[2].localeCompare(b[2]))
         guardar_en_local(array_notas)
         mostrar_de_array(array_notas)
+        boton_ordenar.textContent = "Alfabeticamente"
     }
     else if(ordenar == 2){ //ordenar por color
         array_notas.sort((a,b) => a[5].localeCompare(b[5]))
         guardar_en_local(array_notas)
         mostrar_de_array(array_notas)
+        boton_ordenar.textContent = "Color"
     }
     else if (ordenar == 3){ //ordenar por completadas
         array_notas.sort((a,b) => a[1] - b[1])
         guardar_en_local(array_notas)
         mostrar_de_array(array_notas)
+        boton_ordenar.textContent = "Completada"
     }
 
 });
